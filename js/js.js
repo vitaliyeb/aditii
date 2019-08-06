e9600faabdfad9a90988183a71d64a86a98f8bb8
vitaliyeb <53256472+vitaliyeb@users.noreply.github.com>
2019-08-07 00:06:19 +0500

    Add files via upload
    New bobille

--- js/js.js
+++ js/js.js
@@ -1,15 +1,28 @@
-$('.carousel').carousel({
-  interval: 0 ,
-  touch: true;
-})
-$(document).ready(function(){
-	$(".carousel").swipe( {
-		swipeLeft: function() {
-			$(this).carousel("next");
-		},
-		swipeRight: function() {
-			$(this).carousel("prev");
-		},
-		allowPageScroll: "vertical"
-	});
+
+var slideout = new Slideout({
+  'panel': document.getElementById('panel'),
+  'menu': document.getElementById('menu'),
+  'padding': 256,
+  'tolerance': 70
 });
+
+document.querySelector('.js-slideout-toggle').addEventListener('click', function() {
+  slideout.open();
+});
+
+function close(eve) {
+  eve.preventDefault();
+  slideout.close();
+}
+
+slideout
+  .on('beforeopen', function() {
+    this.panel.classList.add('panel-open');
+  })
+  .on('open', function() {
+    this.panel.addEventListener('click', close);
+  })
+  .on('beforeclose', function() {
+    this.panel.classList.remove('panel-open');
+    this.panel.removeEventListener('click', close);
+  });
